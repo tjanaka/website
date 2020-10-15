36cb110a1f33874e3861163865b0510267793f97
btjanaka <bryon@btjanaka.net>
2020-10-15 00:10:25 -0700

    Ensure compiled-assets are cleaned for build
    Also added placeholder to ensure vendor.js is created.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,7 @@
 // Imports index.scss so that Webpack can detect it.
 import "../css/index.scss";
+
+// Placeholder until we get real code that needs vendor.js.
+Array.from(document.getElementsByTagName("p")).forEach((p) => {
+  console.log(`p ${index}, startsWith('W')`, p, p.innerHTML.startsWith("W"));
+});
