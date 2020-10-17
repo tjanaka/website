f77f17eaa53dddf0d55ca053264b32a3bd0c6003
btjanaka <bryon@btjanaka.net>
2020-10-17 15:20:00 -0700

    Revert "Revert "Add PurgeCSS to remove unused rules""
    This reverts commit 87bba2782093298c99b816d8acf068ad1d812ad1.

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require("autoprefixer")],
+  plugins: [
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["src/**/*.html", "src/**/*.md", "src/**/*.js"],
+    }),
+  ],
 };
