e838a863ba78da47877df390756d33be2a42df27
btjanaka <bryon@btjanaka.net>
2020-10-17 17:55:22 -0700

    Add safelist that seems to work, seems troublesome for now

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,32 @@
 module.exports = {
   plugins: [
-    require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
       content: ["src/**/*.html", "src/**/*.md", "src/**/*.js"],
+      safelist: [
+        "body",
+        "html",
+        "a",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "p",
+        "blockquote",
+        "breadcrumbs",
+        "content",
+        "form",
+        "input",
+        "textarea",
+        "intro",
+        "btn",
+        "loaded",
+        "page-title",
+        "required",
+        "row",
+        "visually-hidden",
+        "menu-visible",
+      ],
     }),
+    require("autoprefixer"),
   ],
 };
